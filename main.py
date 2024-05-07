0ddb24464aedb835e139024823c0619207388bfd
razogarz <patryknapek@gmail.com>
2024-05-07 22:04:22 +0200

    model basis, covering and uncovering, setup

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
-from naiveBoardGen import draw_ascii_board, naive_gen_board
-
+from pythonScripts.boardFunctions import *
 
 def main():
 		"""
 		Generate a naive board with mines
 		"""
 		size = 10
-		number_of_mines = 10
+		number_of_mines = 15
 		first_click = (4, 5)
-		board = naive_gen_board(size, number_of_mines, first_click)
-		draw_ascii_board(board)
+
+		while True:
+			full_board = naive_gen_board(size, number_of_mines, first_click)
+			if full_board[first_click[0]][first_click[1]] == 0:
+				break
+		
+		covered_board = returnBlankBoard(size)
+		draw_ascii_board(full_board)
+		covered_board = uncoverFields(covered_board, full_board, first_click[0], first_click[1])
+		print("\n")
+		draw_ascii_board(covered_board)
+		print(covered_board)
+
+		saveForMinizinc(covered_board, size, first_click)
+		
 	
 if __name__ == "__main__":
 		main()
